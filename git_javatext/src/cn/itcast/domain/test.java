b74f47905947edb58cfab374fa5be3f5d2e3930b
supper <supper@qq.com>
2019-03-14 18:39:47 +0800

    jiazhushi

--- git_javatext/src/cn/itcast/domain/test.java
+++ git_javatext/src/cn/itcast/domain/test.java
@@ -5,8 +5,11 @@ public class test {
     public static void main(String[] args) {
         int a=10;
         System.out.println(a);
+        //这是一行注释
     }
     public void test(){
         System.out.println("ddsf");
+        System.out.println(122222);
     }
+
 }
